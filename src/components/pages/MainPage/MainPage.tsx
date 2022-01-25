8b6a87035866a5d0a215205b8d3e7de167196fdc
Влад Баранюк <vladbaranyuk@gmail.com>
2022-01-25 14:06:59 +0300

    added mui and react-router

---
+++ src/components/pages/MainPage/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Box} from "@mui/material";
+
+export const MainPage = () => {
+  return (
+    <Box>
+      Main Page
+    </Box>
+  );
+};
+
+export default MainPage;
---
+++ src/router/Router.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+    BrowserRouter,
+    Route, Routes,
+} from "react-router-dom";
+import {MainPage} from "../components/pages/MainPage";
+
+
+export const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<MainPage/>} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default Router;
